d5da07dba32af0384c7080f00264dd6b7c243528
tanin9 <taninnusrat199@gmail.com>
2026-03-30 12:59:35 +0600

    basic files are added

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import Board from './components/homepages/Board'
+import Footer from './components/homepages/Footer'
+import Navbar from './components/navbar/Navbar'
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className="btn btn-primary">hello world</div>
+      <Navbar></Navbar>
+      <Board></Board>
+      <Footer></Footer>
+      
+    </>
+  )
+}
+
+export default App
